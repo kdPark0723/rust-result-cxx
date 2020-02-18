e3d3c70b994b106fafee2b085c10bc257f4e713e
박시율 <kdpark0723@gmail.com>
2020-02-18 23:27:57 +0900

    refactoring

--- cmake-build-debug/CMakeFiles/rust_result.dir/cmake_clean.cmake
+++
@@ -1,5 +0,0 @@
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/rust_result.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- cmake-build-debug/CMakeFiles/rust_result.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-file(REMOVE_RECURSE
-  "librust_result.a"
-)
